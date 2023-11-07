eac5b8c540a456ee6dd1fabf31f267c7fd5b8aa5
xinwei_ucc <xinwei_xcc@sina.com>
2023-11-07 19:05:47 +0900

    Test222

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!---test1";
+		return "Greetings from Spring Boot!---test1--test222";
 	}
 
 }
